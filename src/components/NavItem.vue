4adcb86d41637582129087996a260596d821700e
Dan Kim <jidankim@gmail.com>
2018-03-21 19:24:53 +0900

    basic nav items

---
+++ src/components/NavItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="tab">{{ name }}</p>
+</template>
+
+<script>
+export default {
+  props: ['name'],
+};
+</script>
+
+<style scoped>
+.tab {
+  cursor: pointer;
+}
+</style>
--- src/pages/ZaboDetail.vue
+++ src/pages/ZaboDetail.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="zaboDetail">
-    zaboDetail
+    Header Component
+    <div class="headerStyle">
+      <nav-item name="Info"></nav-item>
+      <nav-item name="Review"></nav-item>
+      <nav-item name="신청"></nav-item>
+    </div>
   </div>
 </template>
 
 <script>
+import NavItem from '../components/NavItem';
+
 export default {
+  components: {
+    NavItem,
+  },
 };
 </script>
 
 <style scoped>
+.headerStyle {
+  display: flex;
+  justify-content: space-between;
+  margin: 100px;
+}
 </style>
