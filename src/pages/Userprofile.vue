63ee4db83a481b37a959bbbf45fc03b1d916c5fa
jwoo0122 <thinkratomos@gmail.com>
2018-06-25 16:50:39 +0900

    User Profile branch

---
+++ src/pages/Userprofile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user-profile">
+    user
+  </div>
+</template>
+
+<script>
+export default {
+  name : "userprofile",
+}
+</script>
+
+<style>
+  .user-profile {
+    width : 100px;
+    height : 20px;
+    background-color: coral;
+  }
+</style>
