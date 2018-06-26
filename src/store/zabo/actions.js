17a858708206704cfa3ab9e2dd87fad1484144cf
ujin43255252 <ujin43255252@gmail.com>
2018-06-26 17:17:58 +0900

    eslint_configure

--- src/store/zabo/actions.js
+++ src/store/zabo/actions.js
@@ -1,17 +1,22 @@
 import axios from '@/axios-auth';
 import * as types from '@/store/mutation-types';
 
-const setURL = function(subURL) {
-  return subURL === '' ? '' : typeof(subURL) === 'undefined' ? '' : `${subURL}/`;
+const setURL = function (subURL) {
+  return subURL === '' ? '' : typeof subURL === 'undefined' ? '' : `${subURL}/`;
 };
 
 const actions = {
   zaboesList({ commit, state }, passedPayload) {
     if (!Object.keys(state.zaboesObject).includes(passedPayload.pageNum)) {
-      axios.get(`/zaboes/?page=${passedPayload.pageNum}&page_size=${passedPayload.pageSize}`)
+      axios
+        .get(
+          `/zaboes/?page=${passedPayload.pageNum}&page_size=${
+            passedPayload.pageSize
+          }`,
+        )
         .then((res) => {
           const result = res.data.data;
-          const payload = { result: result, pagenum: passedPayload.pageNum };
+          const payload = { result, pagenum: passedPayload.pageNum };
           commit(types.ZABOES_LIST, payload);
         });
     }
@@ -25,7 +30,8 @@ const actions = {
   // },
   zaboesGetPageCount({ commit }, payload) {
     return new Promise((resolve) => {
-      axios.get(`/zaboes/${setURL(payload.subURL)}?page_size=${payload.pageSize}`)
+      axios
+        .get(`/zaboes/${setURL(payload.subURL)}?page_size=${payload.pageSize}`)
         .then((res) => {
           const result = res.data.page_count;
           commit(types.ZABOES_PAGECOUNT, result);
