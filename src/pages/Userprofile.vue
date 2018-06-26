70589a315f2f4ff674da7ad8e675965efabd6e14
ujin43255252 <ujin43255252@gmail.com>
2018-06-26 17:51:45 +0900

    Userprofile_folder

--- src/pages/Userprofile.vue
+++ src/pages/Userprofile.vue
@@ -3,85 +3,91 @@
     <div class="user-profile">
       프로필
     </div>
-    <img v-bind:src = "imagesrc" class = "profile-image" v-cloak>
+    <img v-bind:src="imagesrc" class="profile-image" v-cloak>
     <p> {{ name }} </p>
     <button v-on:click="tab1">내 정보</button>
     <button v-on:click="tab2">참여한 자보</button>
     <button v-on:click="tab3">찜한 자보</button>
-    <div v-if = "tab === 'tab1'">
+    <div v-if="tab === 'tab1'">
       내 정보
     </div>
-    <div v-else-if = "tab === 'tab2'">
-      참여한 자보
+    <div v-else-if="tab === 'tab2'">
+      <participated></participated>
     </div>
-    <div v-else-if = "tab === 'tab3'">
+    <div v-else-if="tab === 'tab3'">
       찜한 자보
     </div>
- </div>
+
+  </div>
 </template>
 
 <script>
+import Participated from './UserProfile/Participated.vue';
+
 export default {
-  name : "userprofile",
+  name: "userprofile",
   data () {
     return {
-      name : "",
-      imagesrc : "",
-      tab : "tab1"
+      name: "",
+      imagesrc: "",
+      tab: "tab1"
     }
   },
-  methods : {
-    tab1 : function() {
+  methods: {
+    tab1: function () {
       this.tab = 'tab1';
     },
-    tab2 : function() {
+    tab2: function () {
       this.tab = 'tab2';
     },
-    tab3 : function() {
+    tab3: function () {
       this.tab = 'tab3';
     }
+  },
+  component: {
+    'participated': Participated
   },
   created: function () {
     this.$http({
       method: 'get',
       url: 'http://localhost:8000/users/1',
       auth: {
-        username : 'thinkratomos@gmail.com',
-        password : 'Michael5'
+        username: 'thinkratomos@gmail.com',
+        password: 'Michael5'
       }
     })
-    .then((result)=>{
-      this.name = result.data.first_name + " " + result.data.last_name;
-      this.imagesrc = result.data.profile_image;
-      this.data = result.data;
-    })
+      .then((result) => {
+        this.name = result.data.first_name + " " + result.data.last_name;
+        this.imagesrc = result.data.profile_image;
+        this.data = result.data;
+      })
   }
 }
 </script>
 
 <style>
-  [v-cloak] {
-    display:block;
-  }
-  .column {
-    width : 70%;
-    height : 1000px;
-    margin-left : 15%;
-    margin-top:90px;
-  }
+[v-cloak] {
+  display: block;
+}
+.column {
+  width: 70%;
+  height: 1000px;
+  margin-left: 15%;
+  margin-top: 90px;
+}
 
-  .user-profile {
-    width : 100px;
-    height : 20px;
-    text-align: left;
-    font-size: 22pt;
-    font-family: Nanumsquare;
-    font-weight: 800;
-  }
+.user-profile {
+  width: 100px;
+  height: 20px;
+  text-align: left;
+  font-size: 22pt;
+  font-family: Nanumsquare;
+  font-weight: 800;
+}
 
-  .profile-image {
-    height : 100px;
-    width : 100px;
-    border-radius: 100px;
-  }
+.profile-image {
+  height: 100px;
+  width: 100px;
+  border-radius: 100px;
+}
 </style>
---
+++ src/pages/Userprofile/Participated.vue
@@ -0,0 +1,14 @@
+<template lang=''>
+  <div id="participated">
+
+  </div>
+</template>
+<script>
+export default {
+  data () {
+
+  }
+}
+</script>
+<style lang=''>
+</style>
