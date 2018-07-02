344f14fc65fe97266937f010abea48056061e0d8
ujin43255252 <ujin43255252@gmail.com>
2018-07-02 18:16:34 +0900

    Search bar Base

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,23 +1,39 @@
 <template lang=''>
-<input id="searchBar" type="text"/>
+<div id="searchBar" :searchValue="searchValue">
+  <input type="text" class="searchInput">{{searchValue}}</input>
+  <router-link to="/search"><v-icon class="searchIcon">search</v-icon></router-link>
+</div>
 </template>
 <script>
 export default {
   data () {
     return {
-
+      searchValue: ""
     }
   }
 }
 </script>
 <style lang=''>
 #searchBar {
-  width: 16em;
-  min-height: 35px;
+  min-width: 16em;
+  height: 35px;
   border: 2px solid #12397d;
-  text-align: right;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  box-sizing: border-box;
 }
-#searchBar:focus {
+.searchIcon {
+  margin-right: 0.25em;
+}
+.searchInput {
+  border: none;
+  flex: 1;
+  height: 31px;
+  text-align: left;
+  padding-left: 0.75em;
+}
+.searchInput:focus {
   outline: none;
 }
 </style>
