b9e4b661c8465263eda82717e5d4a1d5357fc97d
jwoo0122 <thinkratomos@gmail.com>
2018-07-02 16:33:55 +0900

    Change router setting.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "@/pages/Home";
-import Zabo from "@/pages/Zabo";
 import ZaboDetail from "@/pages/ZaboDetail";
 import ZaboUpdate from "@/pages/ZaboUpdate";
 import ZaboUpload from "@/pages/ZaboUpload";
@@ -16,23 +14,13 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "Home",
-      component: Home
-    },
-    {
-      path: "/zabo",
-      name: "Zabo",
-      component: Zabo
-    },
-    {
-      path: "/zabo/detail/:zabo_id",
-      name: "ZaboDetail",
-      component: ZaboDetail
-    },
-    {
-      path: "/zabo/list",
       name: "ZaboList",
       component: ZaboList
+    },
+    {
+      path: "/zabo/:zabo_id",
+      name: "ZaboDetail",
+      component: ZaboDetail
     },
     {
       path: "/zabo/update",
@@ -45,7 +33,7 @@ export default new Router({
       component: ZaboUpload
     },
     {
-      path: "/zabo/user_profile",
+      path: "/user/profile",
       name: "Zabouserprofile",
       component: ZaboUserprofile
     }
