c75b6e919c71fd989b08cef84228934dce4d8cc8
ujin43255252 <ujin43255252@gmail.com>
2018-07-02 16:18:17 +0900

    Axios auto & Eslint Base

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,14 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: "http://localhost:8000/api"
 });
 
-instance.interceptors.request.use((req) => {
-  // req.headers.Authorization = `ZABO ${localStorage.getItem('token')}`;
-  req.headers.Authorization = 'Basic aHllb25nanVuQGthaXN0LmFjLmtyOmpvMTI2MTI2am8=';
-  // req.headers.ContentType = 'application/json';
-  return req;
-}, error => Promise.reject(error));
+instance.interceptors.request.use(
+  req => {
+    req.headers.Authorization = `ZABO ${localStorage.getItem("token")}`;
+    // req.headers.Authorization = 'Basic aHllb25nanVuQGthaXN0LmFjLmtyOmspvMTI2MTI2am8=';
+    // req.headers.ContentType = 'application/json';
+    return req;
+  },
+  error => Promise.reject(error)
+);
 
 export default instance;
