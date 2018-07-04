6d12b1a9597f5a9f1681a4be75417191bfc652bc
jwoo0122 <thinkratomos@gmail.com>
2018-07-04 20:31:46 +0900

    Login option for SPARCS SSO  or Zabo accoutn added.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,19 +2,44 @@
   <div id="background_div">
     <div id="loginbox">
       <v-app id="vform">
-        <img src="@/logo.svg" id="logo">
-        <v-form method="get" @submit.prevent="login">
-          <v-text-field
-          label = "E-mail"
-          v-model = "email"
-          class = "form"/>
-          <v-text-field
-          label = "Password"
-          v-model = "password"
-          type = "password"
-          class = "form"/>
-          <v-btn type="submit" depressed :loading="loading" color="indigo darken-3" id="login_button">로그인</v-btn>
-        </v-form>
+        <a href="http://localhost:8080">
+          <img src="@/logo.svg" id="logo">
+        </a>
+        <div v-if="isZabologin === false" style="padding-left: 0px;">
+          <v-btn depressed color="indigo darken-3" class="sso-login">
+            SPARCS SSO로 로그인
+          </v-btn>
+          <v-btn depressed color="grey lighten-3" class="zabo-login" @click="zabologin">
+            Zabo 단체 계정으로 로그인
+          </v-btn>
+        </div>
+        <div v-else style="margin-top: -20px;">
+          <div class="backward" @click="zabologin">
+            뒤로
+          </div>
+          <p class="zabologin-text">
+            단체 계정으로 로그인
+          </p>
+          <v-form v-model="valid" method="get" @submit.prevent="login">
+            <v-text-field
+            required
+            label = "E-mail"
+            v-model = "email"
+            :rules="emailRules"
+            class = "field"/>
+            <v-text-field
+            required
+            label = "Password"
+            v-model = "password"
+            :rules="passwordRules"
+            type = "password"
+            class = "field"/>
+            <v-btn type="submit" :disabled="!valid" depressed :loading="loading" color="indigo darken-3" id="login_button">로그인</v-btn>
+          </v-form>
+        </div>
+        <p class="copyright">
+          Copyright 2018. All right reserved.
+        </p>
       </v-app>
     </div>
   </div>
@@ -26,11 +51,21 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      isZabologin: false,
+      valid: true,
+      emailRules: [
+        v => !!v || "이메일을 입력해주세요.",
+        v => /.+@.+/.test(v) || "유효한 이메일 형식이 아닙니다."
+      ],
+      passwordRules: [v => !!v || "비밀번호를 입력해주세요."]
     };
   },
   methods: {
-    login: function() {
+    zabologin() {
+      this.isZabologin = !this.isZabologin;
+    },
+    login() {
       this.$store.commit("START_AJAX");
       axios
         .post("http://localhost:8000/api-token-auth/", {
@@ -41,7 +76,7 @@ export default {
           localStorage.setItem("token", `ZABO ${response.data.token}`);
         })
         .catch(err => {
-          console.log("Wrong Email/password.");
+          console.warn("Wrong Email/password.");
           this.$store.commit("GOT_RESPONSE");
         })
         .then(() => {
@@ -58,7 +93,6 @@ export default {
               this.$store.commit("GOT_RESPONSE");
             })
             .then(() => {
-              console.log("hey");
               this.$emit("logged-in");
             });
         });
@@ -96,12 +130,12 @@ export default {
   transform: translate(-50%, -50%);
   justify-content: center;
   background-color: white;
-  box-shadow: 0px 15px 65px #424242;
+  box-shadow: 0px 15px 65px #1f1f1f;
 }
 
 #vform {
   width: 300px;
-  height: 200px;
+  height: 230px;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -80%);
@@ -120,9 +154,50 @@ export default {
   margin-bottom: 25px;
 }
 
-/* .form {
-  width: 290px;
-} */
+.sso-login {
+  width: 300px;
+  height: 80px;
+  font-size: 14pt;
+  font-family: Nanumsquare;
+  font-weight: 700;
+  color: white;
+  margin-left: 0px;
+}
+
+.zabo-login {
+  width: 300px;
+  height: 80px;
+  font-size: 14pt;
+  font-family: Nanumsquare;
+  font-weight: 700;
+  margin-left: 0px;
+}
+
+.backward {
+  width: 30px;
+  height: 20px;
+  float: left;
+  font-family: Nanumsquare;
+  font-size: 11pt;
+  font-weight: 700;
+  cursor: pointer;
+  position: absolute;
+  margin-top: -5px;
+}
+
+.zabologin-text {
+  width: 300px;
+  text-align: center;
+  font-family: Nanumsquare;
+  color: #12397d;
+  font-size: 13pt;
+  font-weight: 800;
+  margin-top: -10px;
+}
+
+.field {
+  margin-top: 0px;
+}
 
 #login_button {
   width: 300px;
@@ -132,5 +207,13 @@ export default {
   font-family: Nanumsquare;
   font-weight: 700;
   color: white;
+}
+
+.copyright {
+  width: 300px;
+  text-align: center;
+  font-size: 8pt;
+  position: absolute;
+  top: 350px;
 }
 </style>
