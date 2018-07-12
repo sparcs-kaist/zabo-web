abdcd8568d3980f02ca3a3543696824cf845dedc
jwoo0122 <thinkratomos@gmail.com>
2018-07-12 22:12:38 +0900

    Profile Image Upload CSS added.

--- src/pages/Userprofile/Profile.vue
+++ src/pages/Userprofile/Profile.vue
@@ -1,28 +1,36 @@
 <template lang=''>
 <v-app id="template">
   <div class = "whole">
-    <v-btn :disabled="!valid" @click = "edittoggle" :flat="true" :outline="true" :loading = "loading" :color = "color" class="submit" >
-      {{ edit }}
-    </v-btn><br/>
+    <div style="display: flex">
+      <v-btn :disabled="!valid" @click = "edittoggle" depressed :loading = "loading" :color = "color" class="submit_button">
+        {{ edit }}
+      </v-btn>
+      <v-btn v-if = "edit === '저장'" depressed @click = "cancelation" color="red" class="cancel_button">
+        취소
+      </v-btn>
+    </div>
     <p v-if = "edit === '편집'">
       {{ phone }}
       <br>
       {{ nick_name }}
       <br>
+      {{ gender }}
     </p>
-      {{ email }}
-    <v-form v-model = "valid" v-if = "edit === '저장'">
-      <v-text-field
-      label = "전화번호"
-      v-model = "new_phone"
-      class = "textform">
-      </v-text-field>
-      <v-text-field
-      label = "닉네임"
-      v-model = "new_nick_name"
-      class = "textform">
-      </v-text-field>
-    </v-form>
+    <v-layout style="width: 100%; max-width: 900px" row wrap v-if = "edit === '저장'"> 
+      <v-flex xs6>
+        <v-form v-model = "valid">
+          <v-text-field label = "전화번호" v-model = "new_phone" style="width: 90%;">
+          </v-text-field>
+          <v-text-field label = "닉네임" v-model = "new_nick_name" style="width: 90%;">
+          </v-text-field>
+          <v-select :items="genders" v-model="dropdown_selected_gender" label="성별" style="width: 90%;" outline></v-select>
+        </v-form>
+      </v-flex>
+      <v-flex xs6>
+        오른쪽
+      </v-flex>
+    </v-layout>
+    {{ email }}
   </div>
 </v-app>
 </template>
@@ -36,12 +44,15 @@ export default {
       new_first_name: "",
       new_last_name: "",
       new_nick_name: "",
-      new_phone: ""
+      new_phone: "",
+      genders: ["남성", "여성", "기타", "비공개"],
+      dropdown_selected_gender: ""
     };
   },
   created() {
     this.new_nick_name = this.nick_name;
     this.new_phone = this.phone;
+    this.dropdown_selected_gender = this.gender;
   },
   computed: {
     loading() {
@@ -62,34 +73,65 @@ export default {
     email() {
       return this.$store.getters.getEmail;
     },
+    gender() {
+      if (this.$store.getters.getGender == "M") {
+        return "남성";
+      } else if (this.$store.getters.getGender == "F") {
+        return "여성";
+      } else if (this.$store.getters.getGender == "B") {
+        return "비공개";
+      } else if (this.$store.getters.getGender == "E") {
+        return "기타";
+      }
+    },
+    selected_gender() {
+      if (this.dropdown_selected_gender == "남성") {
+        return "M";
+      } else if (this.dropdown_selected_gender == "여성") {
+        return "F";
+      } else if (this.dropdown_selected_gender == "비공개") {
+        return "B";
+      } else if (this.dropdown_selected_gender == "기타") {
+        return "E";
+      }
+    },
     color() {
-      return this.edit === "편집" ? "gray" : "green";
+      return this.edit === "편집" ? "primary" : "green";
     }
   },
   methods: {
-    async edittoggle() {
+    cancelation() {
+      this.new_nick_name = this.nick_name;
+      this.new_phone = this.phone;
+      this.dropdown_selected_gender = this.gender;
+      this.$emit("cancel");
+      this.edit = "편집";
+    },
+    edittoggle() {
       if (this.edit === "편집") {
         this.$emit("editmode");
         this.edit = "저장";
       } else if (this.edit === "저장") {
-        if ((this.image = null)) {
-          var payload = {
-            first_name: this.first,
-            last_name: this.last,
-            nick_name: this.new_nick_name,
-            phone: this.new_phone
-          };
+        if (this.image == null) {
+          var payload = [
+            this.first,
+            this.last,
+            this.new_nick_name,
+            this.new_phone,
+            this.selected_gender
+          ];
           this.$store.dispatch("setMyInfo", payload);
           this.$emit("editmode");
           this.edit = "편집";
         } else {
-          var payload = {
-            first_name: this.first,
-            last_name: this.last,
-            nick_name: this.new_nick_name,
-            phone: this.new_phone,
-            profile: this.image
-          };
+          var payload = [
+            this.first,
+            this.last,
+            this.new_nick_name,
+            this.new_phone,
+            this.selected_gender,
+            this.image
+          ];
           this.$store.dispatch("setMyInfo", payload);
           this.$emit("editmode");
           this.edit = "편집";
@@ -107,22 +149,28 @@ export default {
 .whole {
   width: 100%;
   height: 1000px;
-  text-align: center;
+  display: flex;
+  /* justify-content: center; */
+  flex-direction: column;
+  align-items: center;
 }
 
-.button {
-  display: inline;
-}
-
-.textform {
-  width: 300px;
-}
-
-.submit {
+.submit_button {
   font-family: Nanumsquare;
   font-weight: 700;
   font-size: 12pt;
-  width: 110px;
+  width: 130px;
   height: 40px;
+  color: white;
+}
+
+.cancel_button {
+  float: left;
+  font-family: Nanumsquare;
+  font-weight: 700;
+  font-size: 12pt;
+  height: 40px;
+  color: white;
+  margin-left: -3px;
 }
 </style>
