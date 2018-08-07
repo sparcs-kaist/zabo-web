65abe45c135a5391ced6ded78d1fcb8528e4ab9a
root <ujin43255252@github.com>
2018-08-07 22:41:26 +0900

    Docker Config Files & nginx files configured

---
+++ docker_config/docker_run.sh
@@ -0,0 +1,1 @@
+docker run -it -v /home/sbagi/zabo-web/zabo-web/:/home/zabo/zabo-web/ -p 16139:8000 vk8520/zabo-web-base:0.1
---
+++ docker_config/start_up.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd /home/zabo/zabo-web/nginx_conf
+cp nginx.conf /etc/nginx/sites-available/nginx.conf
+cd /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/nginx.conf nginx.conf
+service nginx start
