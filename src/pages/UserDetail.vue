d9f095df058fa7958e66f94b4c69b95165cd155f
lopun <ujin43255252@gmail.com>
2018-08-17 10:13:54 +0900

    UserDetail Page info added

--- src/pages/UserDetail.vue
+++ src/pages/UserDetail.vue
@@ -1,72 +1,83 @@
 <template>
-  <v-app style="background-color: white">
-    <div id="whole">
-      <div style="width:100%; height:90px;"></div>
-      <div class="user-profile">
-        프로필
+  <div class="totalWrapper">
+    <v-app style="background-color: white">
+      <div id="whole">
+        <div style="width:100%; height:90px;"></div>
+        <div class="user-profile">
+          프로필
+        </div>
+        <div>
+          <img :src="imagesrc" class="profile-image">
+          <p id="name"> {{ first_name + " " + last_name }} </p>
+        </div>
+        <v-tabs fixed-tabs v-model="tab" style="margin-top: 20px; margin-bottom: 20px;">
+          <v-tab :key="1" style="font-size: 17pt; font-weight: 800;">
+            정보
+          </v-tab>
+          <v-tab :key="2" style="font-size: 17pt; font-weight: 800;">
+            업로드한 자보
+          </v-tab>
+          <v-tab :key="3" style="font-size: 17pt; font-weight: 800;">
+            팔로우 하는 유저
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-if="!loading" v-model="tab" style="height: 500px;">
+          <v-tab-item :key="1">
+            <profile :valid="valid" :first="first_name" :last="last_name" :image="profile_image"></profile>
+          </v-tab-item>
+          <v-tab-item :key="2">
+            <div class="zaboListWrapper">
+            </div>
+          </v-tab-item>
+          <v-tab-item :key="3">
+            <div class="followWrapper">
+                <div class="userWrapper" @click="userDetail(user.id)" v-for="(user, index) in following" :key="index">
+                  <div class="userInfoWrapper">
+                    <img :src="user.profile_image" class="userImage">
+                    <span class="userName">{{user.nickName}}</span>
+                  </div>
+                </div>
+                <div class="doesNotExist" v-show="following.length == 0">
+                  <span>{{$t('팔로잉 중인 유저가 없습니다.')}}</span>
+                </div>
+              </div>
+          </v-tab-item>
+        </v-tabs-items>
       </div>
-      <div>
-        <img :src="imagesrc" class="profile-image">
-        <p id="name"> {{ first_name + " " + last_name }} </p>
-      </div>
-      <v-tabs fixed-tabs v-model="tab" style="margin-top: 20px; margin-bottom: 20px;">
-        <v-tab :key="1" style="font-size: 17pt; font-weight: 800;">
-          정보
-        </v-tab>
-        <v-tab :key="2" style="font-size: 17pt; font-weight: 800;">
-          업로드한 자보
-        </v-tab>
-        <v-tab :key="3" style="font-size: 17pt; font-weight: 800;">
-          팔로우 하는 유저
-        </v-tab>
-      </v-tabs>
-      <v-tabs-items v-if="!loading" v-model="tab" style="height: 500px;">
-        <v-tab-item :key="1">
-          <profile :valid="valid" :first="first_name" :last="last_name" :image="profile_image"></profile>
-        </v-tab-item>
-        <v-tab-item v-if="zaboesExist" :key="2">
-          <div class="zaboListWrapper">
-          </div>
-        </v-tab-item>
-        <v-tab-item :key="3">
-          <div>
-            팔로우
-          </div>
-        </v-tab-item>
-      </v-tabs-items>
+    </v-app>
+    <div v-if="modalState" class="zaboModalWrapper">
+      <!-- <zabo-detail-modal :modalZaboData="modalZaboData" @closeModal="closeModal" :zaboId="this.modalZaboId" v-if="modalState"></zabo-detail-modal> -->
     </div>
-  </v-app>
+  </div>
 </template>
 
 <script>
 import axios from "@/axios-auth";
 import Profile from "./Userprofile/Profile";
+import ZaboDetailModal from "@/components/ZaboDetailModal";
+import * as types from "@/store/mutation-types";
 
 export default {
   name: "userDetail",
   created() {
-    axios
-      .get(`/api/users/?search=${this.$route.params.nickName}`)
-      .then(res => res.data.data[0])
-      .then(user => {
-        axios.get(`/api/users/${user.id}/`).then(res => {
-          const {
-            first_name,
-            last_name,
-            gender,
-            joined_date,
-            profile_image,
-            following
-          } = res.data;
-          this.imagesrc = profile_image;
-          this.first_name = first_name;
-          this.last_name = last_name;
-          this.gender = gender;
-          this.joined_date = joined_date;
-          this.following = following;
-          this.loading = false;
-        });
-      });
+    axios.get(`/api/users/${this.$route.params.userId}/`).then(res => {
+      const {
+        first_name,
+        last_name,
+        gender,
+        joined_date,
+        profile_image,
+        following
+      } = res.data;
+      console.log(res);
+      this.imagesrc = profile_image;
+      this.first_name = first_name;
+      this.last_name = last_name;
+      this.gender = gender;
+      this.joined_date = joined_date;
+      this.following = following;
+      this.loading = false;
+    });
   },
   components: {
     profile: Profile
@@ -83,7 +94,8 @@ export default {
       following: [],
       profile_image: null,
       profilePreview: null,
-      loading: true
+      loading: true,
+      modalState: false,
     };
   },
   methods: {
@@ -95,7 +107,22 @@ export default {
     },
     tab3() {
       this.tab = "tab3";
-    }
+    },
+    userDetail(id) {
+      this.$router.push({ name: "UserDetail", params: { userId: id } });
+    },
+    zaboDetail(id, nickname, zaboData) {
+      if (nickname !== "None") {
+        this.modalState = true;
+        this.modalZaboData = zaboData;
+        window.history.pushState(null, null, [`/zabo/${id}`]);
+        this.modalZaboId = id;
+      }
+    },
+    closeModal() {
+      this.modalState = false;
+      window.history.pushState(null, null, [`/user/detail/${this.$route.params.userId}`]);
+    },
   }
 };
 </script>
