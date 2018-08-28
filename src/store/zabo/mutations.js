e642cff6998017099d7ad7b8709748a921c81a89
lopun <ujin43255252@gmail.com>
2018-08-28 18:23:41 +0900

    ZaboUpload & Axios call Error Fixed

--- src/store/zabo/mutations.js
+++ src/store/zabo/mutations.js
@@ -17,16 +17,13 @@ const mutations = {
           author: {
             url: null,
             nickName: "None",
-            profile_image:
-              "https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552&format=original"
+            profile_image: null
           },
           posters: [
             {
               zabo: k + 1,
-              image:
-                "https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552&format=original",
-              image_thumbnail:
-                "https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552&format=original"
+              image: null,
+              image_thumbnail: null
             }
           ],
           created_time: "0000-00-00 00:00",
@@ -46,35 +43,6 @@ const mutations = {
         for (let i = 0; i < filteredZaboes.length; i++) {
           finalZaboes[localMethod].push(filteredZaboes[i]);
         }
-        // if (filteredZaboes.length < payload.pageSize) {
-        //   for (let j = 0; j < payload.pageSize - filteredZaboes.length; j++) {
-        //     finalZaboes[localMethod].push({
-        //       id: filteredZaboes[filteredZaboes.length - 1].id + j + 1,
-        //       founder: {
-        //         url: null,
-        //         nickName: "None",
-        //         profile_image:
-        //           "https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552&format=original"
-        //       },
-        //       posters: [
-        //         {
-        //           zabo:
-        //             filteredZaboes[filteredZaboes.length - 1].id + j + 1,
-        //           image:
-        //             "https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552&format=original",
-        //           image_thumbnail:
-        //             "https://vignette.wikia.nocookie.net/project-pokemon/images/4/47/Placeholder.png/revision/latest?cb=20170330235552&format=original"
-        //         }
-        //       ],
-        //       created_time: "0000-00-00 00:00",
-        //       updated_time: "0000-00-00 00:00",
-        //       like_count: 0,
-        //       title: "None",
-        //       content: "None",
-        //       location: "None"
-        //     });
-        //   }
-        // }
       }
       const currentFinal = finalZaboes[localMethod];
       currentFinal.sort(function(zaboA, zaboB) {
@@ -95,18 +63,9 @@ const mutations = {
       전람회: []
     };
   },
-  // [types.ZABO_UPDATE](state, zabo) {
-  //   function func(x) {
-  //     return x.pk.toString() === zabo.pk.toString();
-  //   }
-  //   state.zaboes.splice(state.zaboes.indexOf(state.zaboes.find(func)), 1, zabo);
-  // },
-  // [types.ZABO_DELETE](state) {
-  //   function func(x) {
-  //     return x.pk.toString() === zabo.pk.toString();
-  //   }
-  //   state.zaboes.splice(state.zaboes.indexOf(state.zaboes.find(func)), 1);
-  // },
+  [types.CATEGORY_ZABOES_RESET](state, payload) {
+    state.zaboes[payload] = [];
+  },
   [types.SET_NOTIFICATIONS](state, payload) {
     state.notifications = payload;
   },
