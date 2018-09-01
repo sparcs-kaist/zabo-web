68082ec91520074225cb522a9761d26893b4f5d9
lopun <ujin43255252@gmail.com>
2018-09-01 15:52:27 +0900

    issue51 fixed(Center Align)

--- docker_config/docker_run.sh
+++ docker_config/docker_run.sh
@@ -1,1 +1,1 @@
-docker run -it -v /home/wheel/docker_deploy/zabo-web/:/home/zabo/zabo-web/ -p 16139:8000 vk8520/zabo-web-base:0.1
+docker run -it -v /mnt/c/Users/lopun/CS/zabo-web/:/home/zabo/zabo-web/ -p 80:8000 vk8520/zabo-web-base:0.1
