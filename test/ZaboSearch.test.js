bcd8d31cb757b4bcf2728a65fee0c1568ca7fe64
lopun <ujin43255252@gmail.com>
2018-10-04 17:32:19 +0900

    ZaboSearch component test added

--- test/ZaboSearch.test.js
+++ test/ZaboSearch.test.js
@@ -1,5 +1,6 @@
 import { shallow } from "vue-test-utils";
 import ZaboSearch from "../src/components/ZaboSearch";
+import axios from "@/axios-auth";
 
 describe("ZaboSearch.test.js", () => {
   let cmp;
@@ -25,16 +26,42 @@ describe("ZaboSearch.test.js", () => {
     });
   });
 
-  it("Props match!", () => {
-    expect(cmp.vm.zaboList).toEqual([]);
+  it("Selected Tab match!", () => {
+    cmp.vm.selectTab(1);
+    expect(cmp.vm.selectedTab).toEqual(1);
+    cmp.vm.selectTab(2);
+    expect(cmp.vm.selectedTab).toEqual(2);
   });
 
-  it("Props match!", () => {
-    expect(cmp.vm.userList).toEqual([]);
+  it("search working!", async () => {
+    // search문 떄림
+    await cmp.vm.searchZaboes();
+
+    // compare할 result 받기
+    const zaboResult = await axios({
+      methods: "get",
+      url: `/api/zaboes/?search=${cmp.vm.searchValue}`
+    }).then(response => response.data.data);
+    // zaboList 테스트
+    expect(cmp.vm.zaboList.length).toEqual(zaboResult.length);
+    expect(cmp.vm.zaboIsLoading).toEqual(false);
+
+    // compare할 result 받기
+    const userResult = await axios({
+      methods: "get",
+      url: `/api/users/?search=${cmp.vm.searchValue}`,
+      headers: {
+        Authorization: sessionStorage.getItem("token")
+      }
+    }).then(response => response.data.data);
+    // zaboList 테스트
+    expect(cmp.vm.userList.length).toEqual(userResult.length);
+    expect(cmp.vm.userIsLoading).toEqual(false);
   });
 
-  it("Props match!", () => {
-    expect(cmp.vm.following).toEqual({});
+  it("closeModal Function working!", () => {
+    cmp.vm.closeModal();
+    expect(cmp.vm.modalState).toEqual(false);
   });
 
   it("has the expected html structure", () => {
